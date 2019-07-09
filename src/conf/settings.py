85d77791d54b1814d3615b543fcb0cd7d0fd9b38
Guilherme Albert <guilherme.albert@codificar.com.br>
2019-07-09 12:03:49 -0300

    Improve app settings

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -1,8 +1,12 @@
+# Importing OS
 import os
 
+# Importing dotenv
 from dotenv import load_dotenv
 
+# Loading dotenv()
 load_dotenv()
 
+# Setting enviroment constants
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 HTTP_URL = os.getenv("BASE_HTTP_URL")
